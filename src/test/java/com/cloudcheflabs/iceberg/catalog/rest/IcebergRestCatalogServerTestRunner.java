95d860785af0288b5a8fdfba4bc7ed0b6c61bcc2
이기동 <mykidong@igidong-ui-MacBookPro.local>
2023-03-31 20:07:04 +0900

    .

--- src/test/java/com/cloudcheflabs/iceberg/catalog/rest/IcebergRestCatalogServerTestRunner.java
+++ src/test/java/com/cloudcheflabs/iceberg/catalog/rest/IcebergRestCatalogServerTestRunner.java
@@ -1,8 +1,19 @@
 package com.cloudcheflabs.iceberg.catalog.rest;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 public class IcebergRestCatalogServerTestRunner {
+
+    @Before
+    public void init() throws Exception {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(IcebergRestCatalogApplication.class, Arrays.asList("").toArray(new String[0]));
+        applicationContext.registerShutdownHook();
+    }
 
     @Test
     public void runServer() throws Exception {
